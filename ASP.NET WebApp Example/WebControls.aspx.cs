28ba06672dc225ab9856932e8caa9d3473faa346
Ratul Ali <rahim498617@gmail.com>
2021-11-29 18:58:20 +0600

    project uploaded

---
+++ ASP.NET WebApp Example/WebControls.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASP.NET_WebApp_Example
+{
+    public partial class WebControls : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HttpCookie cookie = new HttpCookie("student");
+            cookie.Value = "Ratul Ali";
+            Response.Cookies.Add(cookie);
+            var co_val = Response.Cookies["student"].Value;
+            Label1.Text = co_val;
+        }
+    }
+}
